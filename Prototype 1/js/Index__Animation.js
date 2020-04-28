04078312f42b3b14bbc9be304415947118bede42
sunnytarawade <sunnytarawade000@gmail.com>
2020-04-28 18:58:56 +0530

    Prototype 1 Uploaded

---
+++ Prototype 1/js/Index__Animation.js
@@ -0,0 +1,58 @@
+var controller = new ScrollMagic.Controller()
+
+var tl__About = new TimelineMax()
+
+tl__About
+
+.from(".About__Heading",1,{
+    x : -500,
+    y: - 500,
+    ease:Power2.easeOut
+})
+
+.staggerFrom(".Nav a",0.5,{
+    y : -200,
+    ease:Power4.easeOut
+},0.1)
+
+.fromTo(".Underline",2,{
+    width:0,
+    ease:Power3.easeOut,
+    // opacity : 0
+},
+{
+    width : "100%",
+    // opacity : 1
+})
+
+
+
+.fromTo(".Nebula__BG",2,{
+    opacity : 0,
+    borderRadius : "100%",
+    width : 0,
+    height: 0,
+    ease:Elastic.EaseOut
+},{
+    opacity : 1,
+    borderRadius : 0,
+    width : "100%",
+    height: "100%"
+})
+
+var tl__Astronaut__Animation = new TimelineMax()
+
+tl__Astronaut__Animation
+    .from(".Astronaut__BG",5,{
+        top : 0,
+        left: "10vw",
+        opacity : 0,
+        
+    })
+    .from(".Moon__BG",3,{
+        y : "20vw",
+        opacity:0
+    },2.5)
+    .from(".Flag",0.5,{
+        height : 0
+    })
---
+++ Prototype 1/js/index.js
@@ -0,0 +1,14 @@
+particlesJS.load('bg-stars', './js/stars.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+particlesJS.load('bg-stars-2', './js/stars.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+/* particlesJS.load('bg-shootingStars', './js/shootingStars.json', function() {
+  console.log('callback - particles.js config loaded Shooting');
+}); */
+
+
+/* Parallax Effect */
+
+// var rellax = new Rellax('.rellax');
